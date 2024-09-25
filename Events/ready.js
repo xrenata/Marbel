28e10022e557d1a9b0d22b7da721e788e5aa4212
emirhan <emirhan@duck.com>
2024-09-25 22:38:56 +0000

    Refactor presence settings update

--- Events/ready.js
+++ Events/ready.js
@@ -3,10 +3,9 @@ const chalk = require('chalk');
 
 module.exports = {
 	name: 'ready',
-	once: true,
 	execute(client) {
 		client.log('info', `Logged in as ${chalk.red(client.user.tag)}`);
-        client.user.setActivity(client.config.status.text, { type: ActivityType[client.config.status.type] });
-        client.user.setStatus(client.config.status.activity);
+        client.user.setActivity(client.config.status.text, { type: ActivityType[client.config.status.activity] });
+        client.user.setStatus(client.config.status.type);
 	},
 };
