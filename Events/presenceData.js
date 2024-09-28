7f2e40d863bbb112573736c589b23edc6ecb8fe1
emirhan <emirhan@duck.com>
2024-09-28 10:36:02 +0300

    Refactor presenceData.js to save Spotify activity only if it's a different track

--- Events/presenceData.js
+++ Events/presenceData.js
@@ -3,20 +3,28 @@ const chalk = require('chalk');
 module.exports = {
     name: 'presenceUpdate',
     once: false,
-    execute(oldPresence, newPresence) {
+    async execute(oldPresence, newPresence) {
         const user = newPresence.user;
         const spotifyActivity = newPresence.activities.find(activity => activity.name === 'Spotify');
 
         if (spotifyActivity) {
-            new Spotify({
-                userId: user.id,
-                trackName: spotifyActivity.details,
-                artistName: spotifyActivity.state,
-                albumName: spotifyActivity.assets.largeText,
-                albumCover: spotifyActivity.assets.largeImageURL(),
-                playedAt: Date.now()
-            }).save();
-            newPresence.client.log('spotify', `${chalk.blue(user.tag)} is listening to ${chalk.blue(spotifyActivity.details)} by ${chalk.blue(spotifyActivity.state)}`);
+            const lastSpotifyActivity = await Spotify.findOne({ userId: user.id }).sort({ playedAt: -1 });
+            const isSameTrack = lastSpotifyActivity &&
+                lastSpotifyActivity.trackName === spotifyActivity.details &&
+                lastSpotifyActivity.artistName === spotifyActivity.state;
+
+            if (!isSameTrack) {
+                new Spotify({
+                    userId: user.id,
+                    trackName: spotifyActivity.details,
+                    artistName: spotifyActivity.state,
+                    albumName: spotifyActivity.assets.largeText,
+                    albumCover: spotifyActivity.assets.largeImageURL(),
+                    playedAt: Date.now()
+                }).save();
+
+                newPresence.client.log('spotify', `${chalk.blue(user.tag)} is listening to ${chalk.blue(spotifyActivity.details)} by ${chalk.blue(spotifyActivity.state)}`);
+            }
         }
     }
 };
